2d305437c2504c6b178ca370ca0f0cea1f0b4340
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-07-07 16:03:27 +0300

    Добавление файла конфигурации для Boost.Test

--- tests/min_max_finder_tests.cpp
+++ tests/min_max_finder_tests.cpp
@@ -1,6 +1,4 @@
-#define BOOST_TEST_MODULE MinMaxFinderTests
-
-
+#define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
 #include <cstdint>
 
@@ -14,26 +12,26 @@ BOOST_AUTO_TEST_SUITE(MinMaxFinderTests)
 BOOST_AUTO_TEST_CASE(find_min_digit_test) {
     MinMaxFinder minMax;
 
-    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(0)) == static_cast<uint8_t>(0));
-    BOOST_CHECK(minMax.find_min_digit(static_cast<uint32_t>(8)) == static_cast<uint8_t>(8));
-    BOOST_CHECK(minMax.find_min_digit(static_cast<uint32_t>(12345678)) == static_cast<uint8_t>(1));
-    BOOST_CHECK(minMax.find_min_digit(static_cast<uint32_t>(87654321)) == static_cast<uint8_t>(1));
-    BOOST_CHECK(minMax.find_min_digit(static_cast<uint32_t>(87654300)) == static_cast<uint8_t>(0));
-    BOOST_CHECK(minMax.find_min_digit(static_cast<uint32_t>(30)) == static_cast<uint8_t>(0));
-    BOOST_CHECK(minMax.find_min_digit(static_cast<uint32_t>(4534023)) == static_cast<uint8_t>(0));
+    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(0)) == static_cast<uint8_t>(0) );
+    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(8)) == static_cast<uint8_t>(8) );
+    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(12345678)) == static_cast<uint8_t>(1) );
+    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(87654321)) == static_cast<uint8_t>(1) );
+    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(87654300)) == static_cast<uint8_t>(0) );
+    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(30)) == static_cast<uint8_t>(0) );
+    BOOST_CHECK( minMax.find_min_digit(static_cast<uint32_t>(4534023)) == static_cast<uint8_t>(0) );
 }
 
 
 BOOST_AUTO_TEST_CASE(find_max_digit_test) {
     MinMaxFinder minMax;
 
-    BOOST_TEST(minMax.find_max_digit(static_cast<uint32_t>(0)) == static_cast<uint8_t>(0));
-    BOOST_TEST(minMax.find_max_digit(static_cast<uint32_t>(8)) == static_cast<uint8_t>(8));
-    BOOST_TEST(minMax.find_max_digit(static_cast<uint32_t>(12345678)) == static_cast<uint8_t>(8));
-    BOOST_TEST(minMax.find_max_digit(static_cast<uint32_t>(87654321)) == static_cast<uint8_t>(8));
-    BOOST_TEST(minMax.find_max_digit(static_cast<uint32_t>(99994321)) == static_cast<uint8_t>(9));
-    BOOST_TEST(minMax.find_max_digit(static_cast<uint32_t>(30)) == static_cast<uint8_t>(3));
-    BOOST_TEST(minMax.find_max_digit(static_cast<uint32_t>(4538093)) == static_cast<uint8_t>(9));
+    BOOST_TEST( minMax.find_max_digit(static_cast<uint32_t>(0)) == static_cast<uint8_t>(0) );
+    BOOST_TEST( minMax.find_max_digit(static_cast<uint32_t>(8)) == static_cast<uint8_t>(8) );
+    BOOST_TEST( minMax.find_max_digit(static_cast<uint32_t>(12345678)) == static_cast<uint8_t>(8) );
+    BOOST_TEST( minMax.find_max_digit(static_cast<uint32_t>(87654321)) == static_cast<uint8_t>(8) );
+    BOOST_TEST( minMax.find_max_digit(static_cast<uint32_t>(99994321)) == static_cast<uint8_t>(9) );
+    BOOST_TEST( minMax.find_max_digit(static_cast<uint32_t>(30)) == static_cast<uint8_t>(3) );
+    BOOST_TEST( minMax.find_max_digit(static_cast<uint32_t>(4538093)) == static_cast<uint8_t>(9) );
 }
 
 
