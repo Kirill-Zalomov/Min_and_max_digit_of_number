c6f260607715989bfa1a60a9c124e6eb011fc466
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-07-09 18:04:27 +0300

    Выполнена декомпозиция метода run в классе MinMaxConsoleApplication

--- inc/min_max_console_application.hpp
+++ inc/min_max_console_application.hpp
@@ -15,9 +15,29 @@
 
 class MinMaxConsoleApplication : private Application {
 
+    // Поля
+    private: ConsoleInputCotroller console_;
+    private: NumericInputValidator input_validator_;
+    private: InputParser input_parser_;
+    private: MinMaxFinder min_max_finder_;
+    private: std::string user_input_;
+    private: uint32_t number_;
+    private: uint8_t  max_digit_;
+    private: uint8_t  min_digit_;
+
+
+    // Конструкторы, деструкторы и операторы
+    public: MinMaxConsoleApplication();
+
+
     // Методы
-    private: void init() override;
     public: int run() override;
+
+    private: void init() override;
+    private: void print_invitation_message();
+    private: void get_user_input();
+    private: void process_user_input();
+    private: void print_result();
 
 };
 
--- src/min_max_console_application.cpp
+++ src/min_max_console_application.cpp
@@ -1,68 +1,74 @@
 #include "min_max_console_application.hpp"
 
 
+MinMaxConsoleApplication::MinMaxConsoleApplication() {
+    init();
+}
+
+
 void MinMaxConsoleApplication::init() {
     initscr();
 }
 
 
-int MinMaxConsoleApplication::run() {
-    init();
+void MinMaxConsoleApplication::print_invitation_message() {
+    console_.print_message("Print number: ");
+}
 
-    ConsoleInputCotroller console;
-    NumericInputValidator inputValidator;
-    InputParser inputParser;
-    MinMaxFinder minMaxFinder;
 
-    char buffer[8] {0};
-    char current_char {0};
+void MinMaxConsoleApplication::get_user_input() {
+
+    char buffer[8] {0}, current_char {0};
     size_t digits_in_buffer {0};
 
-    console.print_message("Print number: ");
-
     while( digits_in_buffer < 8 ) {
-        current_char = console.get_symbol_without_echo();
-        if(  current_char == '0' && digits_in_buffer == 0 ) {
-            continue;
-        }
-        else if ( inputValidator.is_digit( current_char ) ) {
-            console.print_symbol(current_char);
+        current_char = console_.get_symbol_without_echo();
+
+        if ( current_char == ENTER && digits_in_buffer > 0 ) break;
+        else if(  current_char == '0' && digits_in_buffer == 0 ) continue;
+        else if ( input_validator_.is_digit( current_char ) ) {
+            console_.print_symbol(current_char);
             buffer[digits_in_buffer] = current_char;
             digits_in_buffer++;
         }
         else if ( current_char == BACKSPACE && digits_in_buffer > 0 ) {
-            console.print_symbol(BACKSPACE);
+            console_.print_symbol(BACKSPACE);
             buffer[digits_in_buffer] = '\0';
             digits_in_buffer--;
         }
-        else if ( current_char == ENTER && digits_in_buffer > 0 ) {
-            break;
-        }
     }
 
-    std::string user_input { buffer };
-    uint32_t number {0};
-    uint8_t  max_digit {0};
-    uint8_t  min_digit {0};
+    std::string result { buffer };
+    user_input_ = std::move(result);
+}
 
-    inputValidator.does_input_have_right_size(user_input, 1, 8);
-    inputValidator.is_input_correct(user_input);
 
-    number = inputParser.parse_string_to_uint32(user_input);
-    max_digit = minMaxFinder.find_max_digit(number);
-    min_digit = minMaxFinder.find_min_digit(number);
+void MinMaxConsoleApplication::process_user_input() {
+    input_validator_.does_input_have_right_size(user_input_, 1, 8);
+    input_validator_.is_input_correct(user_input_);
 
-    console.print_symbol('\n');
-    console.print_message("Min digit: ");
-    console.print_symbol(static_cast<char>(min_digit + '0'));
-    console.print_symbol('\n');
-    console.print_message("Max digit: ");
-    console.print_symbol(static_cast<char>(max_digit + '0'));
-    console.print_symbol('\n');
+    number_ = input_parser_.parse_string_to_uint32(user_input_);
+    max_digit_ = min_max_finder_.find_max_digit(number_);
+    min_digit_ = min_max_finder_.find_min_digit(number_);
+}
 
-    console.print_message("\n\nPress any key to exit..\n\n");
-    console.get_symbol_without_echo();
+
+void MinMaxConsoleApplication::print_result() {
+    console_.print_message("\nMin digit: ");
+    console_.print_symbol(static_cast<char>(min_digit_ + '0'));
+    console_.print_message("\nMax digit: ");
+    console_.print_symbol(static_cast<char>(max_digit_ + '0'));
+
+    console_.print_message("\n\n\nPress any key to exit..\n\n");
+    console_.get_symbol_without_echo();
+}
+
+
+int MinMaxConsoleApplication::run() {
+    print_invitation_message();
+    get_user_input();
+    process_user_input();
+    print_result();
 
     return 0;
 }
-
