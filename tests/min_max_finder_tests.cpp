9ceef9a4860a6a7c442f2995a4cb69d9636b8ca0
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-07-09 19:43:28 +0300

    Исправлены тесты для класса MinMaxFinder под BOOST_DATA_TEST_CASE

--- tests/min_max_finder_tests.cpp
+++ tests/min_max_finder_tests.cpp
@@ -1,36 +1,53 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
 
 
 #include "min_max_finder.hpp"
 
 
+namespace bdata = boost::unit_test::data;
+
+
 BOOST_AUTO_TEST_SUITE(MinMaxFinderTests)
 
 
-BOOST_AUTO_TEST_CASE(find_min_digit_test) {
+BOOST_DATA_TEST_CASE( find_min_digit_test,
+    bdata::make({
+        // Входное число | Ожидаемая цифра
+        std::make_tuple(0U,          static_cast<uint8_t>(0)),
+        std::make_tuple(8U,          static_cast<uint8_t>(8)),
+        std::make_tuple(12345678U,   static_cast<uint8_t>(1)),
+        std::make_tuple(87654321U,   static_cast<uint8_t>(1)),
+        std::make_tuple(87654300U,   static_cast<uint8_t>(0)),
+        std::make_tuple(30U,         static_cast<uint8_t>(0)),
+        std::make_tuple(4534023U,    static_cast<uint8_t>(0))
+    }),
+    input, expected
+)
+{
     MinMaxFinder mmf;
-
-    BOOST_TEST_CHECK( mmf.find_min_digit(static_cast<uint32_t>(0)) == static_cast<uint8_t>(0) );
-    BOOST_TEST_CHECK( mmf.find_min_digit(static_cast<uint32_t>(8)) == static_cast<uint8_t>(8) );
-    BOOST_TEST_CHECK( mmf.find_min_digit(static_cast<uint32_t>(12345678)) == static_cast<uint8_t>(1) );
-    BOOST_TEST_CHECK( mmf.find_min_digit(static_cast<uint32_t>(87654321)) == static_cast<uint8_t>(1) );
-    BOOST_TEST_CHECK( mmf.find_min_digit(static_cast<uint32_t>(87654300)) == static_cast<uint8_t>(0) );
-    BOOST_TEST_CHECK( mmf.find_min_digit(static_cast<uint32_t>(30)) == static_cast<uint8_t>(0) );
-    BOOST_TEST_CHECK( mmf.find_min_digit(static_cast<uint32_t>(4534023)) == static_cast<uint8_t>(0) );
+    BOOST_CHECK(mmf.find_min_digit(input) == expected);
 }
 
 
-BOOST_AUTO_TEST_CASE(find_max_digit_test) {
+BOOST_DATA_TEST_CASE( find_max_digit_test,
+    bdata::make({
+        // Входное число | Ожидаемая цифра
+        std::make_tuple(0U,          static_cast<uint8_t>(0)),
+        std::make_tuple(8U,          static_cast<uint8_t>(8)),
+        std::make_tuple(12345678U,   static_cast<uint8_t>(8)),
+        std::make_tuple(87654321U,   static_cast<uint8_t>(8)),
+        std::make_tuple(99994321U,   static_cast<uint8_t>(9)),
+        std::make_tuple(30U,         static_cast<uint8_t>(3)),
+        std::make_tuple(4538093U,    static_cast<uint8_t>(9))
+    }),
+    input, expected
+)
+{
     MinMaxFinder mmf;
-
-    BOOST_TEST_CHECK( mmf.find_max_digit(static_cast<uint32_t>(0)) == static_cast<uint8_t>(0) );
-    BOOST_TEST_CHECK( mmf.find_max_digit(static_cast<uint32_t>(8)) == static_cast<uint8_t>(8) );
-    BOOST_TEST_CHECK( mmf.find_max_digit(static_cast<uint32_t>(12345678)) == static_cast<uint8_t>(8) );
-    BOOST_TEST_CHECK( mmf.find_max_digit(static_cast<uint32_t>(87654321)) == static_cast<uint8_t>(8) );
-    BOOST_TEST_CHECK( mmf.find_max_digit(static_cast<uint32_t>(99994321)) == static_cast<uint8_t>(9) );
-    BOOST_TEST_CHECK( mmf.find_max_digit(static_cast<uint32_t>(30)) == static_cast<uint8_t>(3) );
-    BOOST_TEST_CHECK( mmf.find_max_digit(static_cast<uint32_t>(4538093)) == static_cast<uint8_t>(9) );
+    BOOST_CHECK(mmf.find_max_digit(input) == expected);
 }
 
 
