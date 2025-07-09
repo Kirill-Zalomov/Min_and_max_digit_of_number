65cbf1f7d5e621a6b41a9b52ea0234c29685690d
Kirill-Zalomov <kirill.zalomov@gmail.com>
2025-07-09 19:58:47 +0300

    Исправлены тесты для класса NumericInputValidator под BOOST_DATA_TEST_CASE

--- tests/numeric_input_validator_tests.cpp
+++ tests/numeric_input_validator_tests.cpp
@@ -1,87 +1,107 @@
 #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include <boost/test/data/monomorphic.hpp>
 
 
 #include "numeric_input_validator.hpp"
 
 
+namespace bdata = boost::unit_test::data;
+
+
 BOOST_AUTO_TEST_SUITE(NumericInputValidatorTests)
 
 
-BOOST_AUTO_TEST_CASE(is_digit_test) {
+// Тесты для is_digit()
+BOOST_DATA_TEST_CASE( is_digit_true_test,
+    bdata::make({'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'}),
+    digit
+)
+{
     NumericInputValidator niv;
-
-    BOOST_TEST_CHECK( niv.is_digit('0') );
-    BOOST_TEST_CHECK( niv.is_digit('1') );
-    BOOST_TEST_CHECK( niv.is_digit('2') );
-    BOOST_TEST_CHECK( niv.is_digit('3') );
-    BOOST_TEST_CHECK( niv.is_digit('4') );
-    BOOST_TEST_CHECK( niv.is_digit('5') );
-    BOOST_TEST_CHECK( niv.is_digit('6') );
-    BOOST_TEST_CHECK( niv.is_digit('7') );
-    BOOST_TEST_CHECK( niv.is_digit('8') );
-    BOOST_TEST_CHECK( niv.is_digit('9') );
-
-    BOOST_TEST_CHECK( !niv.is_digit('a') );
-    BOOST_TEST_CHECK( !niv.is_digit('!') );
-    BOOST_TEST_CHECK( !niv.is_digit('/') );
-    BOOST_TEST_CHECK( !niv.is_digit(':') );
+    BOOST_CHECK(niv.is_digit(digit));
 }
 
 
-BOOST_AUTO_TEST_CASE(does_input_have_rigth_size_test) {
+BOOST_DATA_TEST_CASE( is_digit_false_test,
+    bdata::make({'a', '!', '/', ':', '@', ' ', '\n', '\t', 'A', '.'}),
+    not_digit
+)
+{
+    NumericInputValidator niv;
+    BOOST_CHECK(!niv.is_digit(not_digit));
+}
+
+
+// Тесты для does_input_have_right_size() с валидными аргументами
+BOOST_DATA_TEST_CASE( does_input_have_right_size_valid_args_test,
+    bdata::make({
+        // input, min, max, expected
+        std::make_tuple("583723", 1, 8, true),
+        std::make_tuple("5", 1, 8, true),
+        std::make_tuple("12345678", 1, 8, true),
+        std::make_tuple("123456789", 9, 9, true),
+        std::make_tuple("", 1, 8, false),
+        std::make_tuple("123456789", 1, 8, false)
+    }),
+    input_str, min_size, max_size, expected
+)
+{
+    NumericInputValidator niv;
+    BOOST_CHECK(niv.does_input_have_right_size(input_str, min_size, max_size) == expected);
+    BOOST_CHECK_NO_THROW(niv.does_input_have_right_size(input_str, min_size, max_size));
+}
+
+
+// Тест для does_input_have_right_size() с невалидными аргументами
+BOOST_AUTO_TEST_CASE(does_input_have_right_size_invalid_args_test) {
     NumericInputValidator niv;
 
-    BOOST_TEST_CHECK( niv.does_input_have_right_size("583723", 1, 8) );
-    BOOST_CHECK_NO_THROW( niv.does_input_have_right_size("583723", 1, 8) );
-
-    BOOST_TEST_CHECK( niv.does_input_have_right_size("5", 1, 8) );
-    BOOST_CHECK_NO_THROW( niv.does_input_have_right_size("5", 1, 8) );
-
-    BOOST_TEST_CHECK( niv.does_input_have_right_size("12345678", 1, 8) );
-    BOOST_CHECK_NO_THROW( niv.does_input_have_right_size("12345678", 1, 8) );
-
-    BOOST_TEST_CHECK( niv.does_input_have_right_size("123456789", 9, 9) );
-    BOOST_CHECK_NO_THROW( niv.does_input_have_right_size("123456789", 9, 9) );
-
-    BOOST_TEST_CHECK( !niv.does_input_have_right_size("", 1, 8) );
-    BOOST_CHECK_NO_THROW( niv.does_input_have_right_size("", 1, 8) );
-
-    BOOST_TEST_CHECK( !niv.does_input_have_right_size("123456789", 1, 8) );
-    BOOST_CHECK_NO_THROW( niv.does_input_have_right_size("123456789", 1, 8) );
-
-    BOOST_REQUIRE_EXCEPTION(
+    BOOST_CHECK_EXCEPTION(
         niv.does_input_have_right_size("", 8, 1),
         std::invalid_argument,
-        [](const std::invalid_argument &exeption) {
-            return std::string(exeption.what()) == "the minimum size cannot be larger than the maximum";
+        [](const std::invalid_argument& ex) {
+            return std::string(ex.what()) == "the minimum size cannot be larger than the maximum";
         }
     );
 }
 
 
-BOOST_AUTO_TEST_CASE(is_input_correct_test) {
+// Тесты для is_input_correct()
+BOOST_DATA_TEST_CASE( is_input_correct_true_test,
+    bdata::make({
+        "0",
+        "1",
+        "123",
+        "1276481748721744327898472781824"
+    }),
+    input_str
+)
+{
     NumericInputValidator niv;
-
-    BOOST_TEST_CHECK( niv.is_input_correct("0") );
-    BOOST_TEST_CHECK( niv.is_input_correct("1") );
-    BOOST_TEST_CHECK( niv.is_input_correct("123") );
-    BOOST_TEST_CHECK( niv.is_input_correct("1276481748721744327898472781824") );
-
-    BOOST_TEST_CHECK( !niv.is_input_correct("") );
-
-    BOOST_TEST_CHECK( !niv.is_input_correct("03") );
-    BOOST_TEST_CHECK( !niv.is_input_correct("000000") );
-    BOOST_TEST_CHECK( !niv.is_input_correct("000123") );
-    BOOST_TEST_CHECK( !niv.is_input_correct("0333330") );
-
-    BOOST_TEST_CHECK( !niv.is_input_correct("12764817487217443.27898472781824") );
-    BOOST_TEST_CHECK( !niv.is_input_correct("12764817487217443,27898472781824") );
-
-    BOOST_TEST_CHECK( !niv.is_input_correct("123a3cb523") );
-    BOOST_TEST_CHECK( !niv.is_input_correct("Hello") );
-    BOOST_TEST_CHECK( !niv.is_input_correct("1H&el@l^o") );
+    BOOST_CHECK(niv.is_input_correct(input_str));
 }
 
+
+BOOST_DATA_TEST_CASE( is_input_correct_false_test,
+    bdata::make({
+        "",
+        "03",
+        "000000",
+        "000123",
+        "0333330",
+        "12764817487217443.27898472781824",
+        "12764817487217443,27898472781824",
+        "123a3cb523",
+        "Hello",
+        "1H&el@l^o"
+    }),
+    input_str
+)
+{
+    NumericInputValidator niv;
+    BOOST_CHECK(!niv.is_input_correct(input_str));
+}
 
 BOOST_AUTO_TEST_SUITE_END()
